f5a7e2c2e4a4273a6a70ed02e2ccf21c6a4d026f
Utkarsh Arora <arorautkarsh2002@gmail.com>
2022-05-02 23:55:43 +0530

    Inversion Count Program Added

---
+++ Count Inversions/NaiveApproach.cpp
@@ -0,0 +1,17 @@
+Class Solution
+{
+    public:
+
+
+    Long Long int inversionCount(Long Long arr[],Long Long n)
+    {
+        int inv_count=0;
+        for(int i=0;i<n-1;i++)
+          for(int j=i+1;j<n;j++)
+              if(arr[i] > arr[j])
+                inv_count++;
+
+        return inv_count;        
+    }
+
+};
