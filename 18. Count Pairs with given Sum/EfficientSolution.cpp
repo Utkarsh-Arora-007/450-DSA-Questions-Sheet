665a1ee775616a3fb8ef0b3b6dc7a923e341dd88
Utkarsh Arora <arorautkarsh2002@gmail.com>
2022-05-04 01:42:47 +0530

    Count pairs with Given Sum Added

---
+++ 18. Count Pairs with given Sum/EfficientSolution.cpp
@@ -0,0 +1,15 @@
+//Function that takes O(n) Time and O(1) Space 
+int getPairsCount(int a[], int n, int k)
+{
+    unordered_map<int ,int> m;
+    int c=0;
+    for(int i=0;i<n;i++)
+    {
+        c+=m[k-a[i]];
+        m[a[i]]++;
+
+    }
+
+    return c;
+
+}
